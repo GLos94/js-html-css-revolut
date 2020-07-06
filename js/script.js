cf959d71e10545db88fc82acb0871344e6d58dbd
Giulia <giulialosito94@gmail.com>
2020-07-06 20:08:04 +0200

    Prova js-revolut

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-console.log("ok");
-$(document).ready(function() {
-  $('.menu-navbar li').mouseenter(function(){
-    $('.dropdown-square').toggle();
-  });
+// console.log("ok");
+ $(document).ready(function() {
 
-  
-});
+  var itemCliccato = $('.menu-navbar li');
+  var dropActive = $('.dropdown-square.active');
+   itemCliccato.click(function () {
+     dropActive.removeClass('active');
+     $(this).siblings().addClass('active');
+     console.log(dropActive);
+   })
+
+   if (dropActive.hasClass('active')) {
+     itemCliccato.mouseenter(function () {
+         $('.dropdown-square').toggle();
+       });
+
+   }
+
+ });
+
+
+  // $(document).ready(function() {
+  //   $('.menu-navbar li').mouseenter(function() {
+  //     $('.dropdown-square').toggle();
+  //   });
+  // });
