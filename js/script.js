1d6f1183fe1f0a4f39b2439c56549231eba48992
Giulia <giulialosito94@gmail.com>
2020-07-07 10:42:45 +0200

    Prove js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-// console.log("ok");
- $(document).ready(function() {
+$(document).ready(function() {
 
   var itemCliccato = $('.menu-navbar li');
   var dropActive = $('.dropdown-square.active');
-   itemCliccato.click(function () {
-     dropActive.removeClass('active');
-     $(this).siblings().addClass('active');
-     console.log(dropActive);
+  itemCliccato.click(function () {
+  dropActive.removeClass('active');
+    $(this).siblings().addClass('active');
+    console.log(dropActive);
    })
 
    if (dropActive.hasClass('active')) {
      itemCliccato.mouseenter(function () {
-         $('.dropdown-square').toggle();
-       });
-
+        $('.dropdown-square').toggle();
+      });
    }
+
+   $('dropdown-square').eq();
+   console.log('dropdown-square'.length);
 
  });
 
 
-  // $(document).ready(function() {
-  //   $('.menu-navbar li').mouseenter(function() {
-  //     $('.dropdown-square').toggle();
-  //   });
-  // });
+   // $(document).ready(function() {
+   //  $('.menu-navbar li').mouseenter(function() {
+   //    $('.dropdown-square').toggle();
+   //   });
+   // });
